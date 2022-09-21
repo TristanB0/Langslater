2b87f6faeaa1dabd5090bd0726185abd037b9f73
Tristan BONY <tristan.bony@hotmail.fr>
2022-09-21 21:06:11 +0200

    Intents + help command

--- main.py
+++ main.py
@@ -19,3 +19,25 @@ cur.execute("""CREATE TABLE IF NOT EXISTS languages (
 				language VARCHAR NOT NULL,
                 PRIMARY KEY (guild_id));""")
 con.commit()
+
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+
+
+intents = discord.Intents.none()
+intents.guilds = True
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+tree = app_commands.CommandTree(client)
+
+@tree.command(name="help", description="Get help")
+async def help(interaction: discord.Interaction):
+    await interaction.response.send_message("""
+    /help - Show this message \n
+    /set_languages [string] - Set the languages for which the bot will NOT translate the message \n
+    Made by Tristan BONY --> https://www.tristanbony.me
+    """, ephemeral=True)
