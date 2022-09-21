ed01b7e253faeb48d81e365021d745c625d88b6c
Tristan BONY <tristan.bony@hotmail.fr>
2022-09-21 21:02:35 +0200

    Made DB

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import sqlite3
 from os import getenv
 
 import discord
@@ -6,3 +7,15 @@ import requests
 from discord import app_commands
 from dotenv import load_dotenv
 
+load_dotenv()
+discord_token = getenv("DISCORD_TOKEN")
+deepl_auth_key = getenv("DEEPL_AUTH_KEY")
+
+con = sqlite3.connect("database.db3")
+cur = con.cursor()
+cur.execute("drop table user;")    # for debugging only
+cur.execute("""CREATE TABLE IF NOT EXISTS languages (
+                guild_id INTEGER NOT NULL,
+				language VARCHAR NOT NULL,
+                PRIMARY KEY (guild_id));""")
+con.commit()
