3708e0ae2252a33fc29dbe848b1a54d00387b459
Tristan BONY <tristan.bony@hotmail.fr>
2023-01-17 10:23:27 +0100

    I forgot what I've done

--- main.py
+++ main.py
@@ -10,11 +10,39 @@ from discord import app_commands
 from discord.app_commands import Choice
 from dotenv import load_dotenv
 
-
 load_dotenv()
 discord_token = getenv("DISCORD_TOKEN")
 deepl_auth_key = getenv("DEEPL_AUTH_KEY")
 
+languages = [
+    Choice(name="Bulgarian", value="BG"),
+    Choice(name="Chinese", value="ZH"),
+    Choice(name="Czech", value="CS"),
+    Choice(name="Danish", value="DA"),
+    Choice(name="Dutch", value="NL"),
+    Choice(name="English", value="EN"),
+    # Choice(name="Estonian", value="ET"),
+    Choice(name="Finnish", value="FI"),
+    Choice(name="French", value="FR"),
+    Choice(name="German", value="GE"),
+    Choice(name="Greek", value="EL"),
+    Choice(name="Hungarian", value="HU"),
+    Choice(name="Indonesian", value="ID"),
+    Choice(name="Italian", value="IT"),
+    Choice(name="Japanese", value="JA"),
+    # Choice(name="Latvian", value="LV"),
+    Choice(name="Lithuanian", value="LT"),
+    Choice(name="Polish", value="PL"),
+    Choice(name="Portuguese", value="PT"),
+    Choice(name="Romanian", value="RO"),
+    Choice(name="Russian", value="RU"),
+    Choice(name="Slovak", value="SK"),
+    Choice(name="Slovenian", value="SL"),
+    Choice(name="Spanish", value="ES"),
+    Choice(name="Swedish", value="SV"),
+    Choice(name="Turkish", value="TR"),
+    Choice(name="Ukrainian", value="UK")
+]
 
 con = sqlite3.connect("database.db3")
 cur = con.cursor()
@@ -38,7 +66,7 @@ class MyClient(discord.Client):
         self.synced = False
 
     async def setup_hook(self) -> None:
-        super.bg_task = self.loop.create_task(self.new_log())
+        self.bg_task = self.loop.create_task(self.new_log())
 
     async def on_ready(self):
         await self.wait_until_ready()
@@ -69,7 +97,9 @@ class MyClient(discord.Client):
                 text_translated = translator.translate_text(message.content, target_lang=translation_language[0])
 
                 if text_translated.detected_source_lang != translation_language[0]:
-                    await message.reply("{0} said in {1}: \n\n \"{2}\"".format(message.author, text_translated.detected_source_lang, text_translated.text))
+                    await message.reply(
+                        "{0} said in {1}: \n\n \"{2}\"".format(message.author, text_translated.detected_source_lang,
+                                                               text_translated.text))
         else:
             await message.reply("Sorry, I am not able to translate this at the moment :'(")
             print("Character usage count: {0}".format(deepl_usage.character))
@@ -80,8 +110,10 @@ class MyClient(discord.Client):
         now = datetime.datetime.now()
         # s_logger = logging.StreamHandler()
         # f_logger = logging.FileHandler(filename="logs/{0}.log".format(now.strftime("%Y-%m-%d %H:%M:%S")), encoding="utf-8")
-        logging.basicConfig(filename="logs/{0}.log".format(now.strftime("%Y-%m-%d %H:%M:%S")), encoding="utf-8", level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
-        await asyncio.sleep(86400)     # Wait a day
+        logging.basicConfig(filename="logs/{0}.log".format(now.strftime("%Y-%m-%d %H:%M:%S")), encoding="utf-8",
+                            level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s',
+                            datefmt="%Y-%m-%d %H:%M:%S")
+        await asyncio.sleep(86400)  # Wait a day
         # Things to do here
 
 
@@ -106,45 +138,21 @@ async def help(interaction: discord.Interaction):
 
 
 @tree.command(name="set_language", description="Set the language you don't want to be translated")
-@app_commands.choices(language=[
-    Choice(name="Bulgarian", value="BG"),
-    Choice(name="Chinese", value="ZH"),
-    Choice(name="Czech", value="CS"),
-    Choice(name="Danish", value="DA"),
-    Choice(name="Dutch", value="NL"),
-    Choice(name="English", value="EN"),
-    # Choice(name="Estonian", value="ET"),
-    Choice(name="Finnish", value="FI"),
-    Choice(name="French", value="FR"),
-    Choice(name="German", value="GE"),
-    Choice(name="Greek", value="EL"),
-    Choice(name="Hungarian", value="HU"),
-    Choice(name="Indonesian", value="ID"),
-    Choice(name="Italian", value="IT"),
-    Choice(name="Japanese", value="JA"),
-    # Choice(name="Latvian", value="LV"),
-    Choice(name="Lithuanian", value="LT"),
-    Choice(name="Polish", value="PL"),
-    Choice(name="Portuguese", value="PT"),
-    Choice(name="Romanian", value="RO"),
-    Choice(name="Russian", value="RU"),
-    Choice(name="Slovak", value="SK"),
-    Choice(name="Slovenian", value="SL"),
-    Choice(name="Spanish", value="ES"),
-    Choice(name="Swedish", value="SV"),
-    Choice(name="Turkish", value="TR"),
-    Choice(name="Ukrainian", value="UK")
-])
+@app_commands.choices(language=languages)
 async def set_language(interaction: discord.Interaction, language: Choice[str]):
-    cur.execute("INSERT OR REPLACE INTO languages (guild_id, language) VALUES (?, ?);", (interaction.guild.id, language.value))
+    cur.execute("INSERT OR REPLACE INTO languages (guild_id, language) VALUES (?, ?);",
+                (interaction.guild.id, language.value))
     con.commit()
 
     await interaction.response.send_message("You have added {0} language.".format(language.name), ephemeral=True)
 
 
 @tree.command(name="translate", description="Send a translated text")
+@app_commands.choices(language=languages)
 async def translate(interaction: discord.Interaction, language: Choice[str], text: str):
-    await interaction.response.send_message("{0} wants to say something in {1}:\n{2}".format(interaction.user.mention, language.name, translator.translate_text(text, target_lang=language.value)))
+    await interaction.response.send_message(
+        "{0} wants to say something in {1}:\n{2}".format(interaction.user.mention, language.name,
+                                                         translator.translate_text(text, target_lang=language.value)))
 
 
 client.run(discord_token)
